bf214d9f972a5c0ce6e3dac08a2ca027cdb43508
aruzen <aruzenueii@gmail.com>
2025-11-04 12:38:17 +0900

    Use official react-router-dom

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,8 @@
-import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      'react-router-dom': fileURLToPath(new URL('./src/lib/simple-router.tsx', import.meta.url)),
-    },
-  },
   server: {
     host: true,
     port: 3000,
